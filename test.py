33d1ad36576db6c361ac7b5df106cf15391bd3fd
Ana Belem Romero Jiménez <ada_hack579@womenintech.mx>
2024-10-05 10:04:58 -0600

    Agragmos pruebas de funciones

---
+++ test.py
@@ -0,0 +1,17 @@
+def suma(a, b):
+    return a + b
+
+def resultado(adicion):
+    return print ("a + b = " + str (adicion) )
+
+print (suma(3,4))
+
+resultado(suma(4,5))
+
+def vic(comida, bebida):
+    print ("Vic comió " + comida + " " + "y bebió " + " " + bebida)
+    return print ("La " + comida + " " + "se volvio caquita.")
+
+vic("croquetas", "agua")
+vic("salchichas", "jugo")
+vic("cinco manzanas", "leche")
